f0cf3105674b48b3286a55b690d36b959fac99b1
Niki Vazou <nikivazou@gmail.com>
2019-01-09 16:56:53 +0100

    imports confusion

---
+++ tests/import/A.hs
@@ -0,0 +1,6 @@
+module A where 
+
+import B 
+
+a :: Eq a => B a -> Bool 
+a (B x y) = x == y 
---
+++ tests/import/B.hs
@@ -0,0 +1,6 @@
+module B where 
+
+import C
+
+data B a = B {b1 :: a, b2 :: a }
+{-@ data B a = B {b1 :: a, b2 :: {v:a | cProp b1 v } } @-}
